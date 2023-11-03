939b104904b469cdc74ef40b6d83c02ee678e2a2
alexbr-alves <alexbr.alves@gmail.com>
2023-11-03 16:01:50 -0300

    configure Bottom navigation

--- app/src/main/java/alex/lop/io/alexProject/MainActivity.kt
+++ app/src/main/java/alex/lop/io/alexProject/MainActivity.kt
@@ -3,14 +3,32 @@ package alex.lop.io.alexProject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import alex.lop.io.alexProject.databinding.ActivityMainBinding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navHostFragment : NavHostFragment
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews(binding)
+    }
+
+    private fun initViews(binding : ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {
+
+            }
+        }
     }
 }
