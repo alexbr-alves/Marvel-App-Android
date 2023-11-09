24e5bd44104f043c6cbf306ccb4b941338456d10
alexbr-alves <alexbr.alves@gmail.com>
2023-11-08 22:48:40 -0300

    models created

---
+++ app/src/main/java/alex/lop/io/alexProject/data/model/serie/SeriesModelData.kt
@@ -0,0 +1,9 @@
+package alex.lop.io.alexProject.data.model.serie
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class SeriesModelData(
+    @SerializedName("results")
+    val result: List<SeriesModel>
+) : Serializable
