9b66ff4b9e585a078c982f5649aa00d97acbee4d
alexbr-alves <alexbr.alves@gmail.com>
2023-11-12 19:13:50 -0300

    insert about session

--- app/src/main/java/alex/lop/io/alexProject/adapters/DetailsCharacterAdapter.kt
+++ app/src/main/java/alex/lop/io/alexProject/adapters/DetailsCharacterAdapter.kt
@@ -2,6 +2,7 @@ package alex.lop.io.alexProject.adapters
 
 
 import alex.lop.io.alexProject.fragment.detailsCharacter.ComicsCharacterFragment
+import alex.lop.io.alexProject.fragment.detailsCharacter.DescriptionCharacterFragment
 import alex.lop.io.alexProject.fragment.detailsCharacter.EventsCharacterFragment
 import alex.lop.io.alexProject.fragment.detailsCharacter.SeriesCharacterFragment
 import alex.lop.io.alexProject.fragment.detailsCharacter.StoriesCharacterFragment
@@ -10,17 +11,18 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class DetailsCharacterAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, val id: Int) :
+class DetailsCharacterAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, val id: Int, val description: String) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
 
-    override fun getItemCount(): Int = 4
+    override fun getItemCount(): Int = 5
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> ComicsCharacterFragment(id)
-            1 -> EventsCharacterFragment(id)
-            2 -> SeriesCharacterFragment(id)
-            3 -> StoriesCharacterFragment(id)
+            0 -> DescriptionCharacterFragment(description)
+            1 -> ComicsCharacterFragment(id)
+            2 -> EventsCharacterFragment(id)
+            3 -> SeriesCharacterFragment(id)
+            4 -> StoriesCharacterFragment(id)
             else -> ComicsCharacterFragment(id)
         }
     }
